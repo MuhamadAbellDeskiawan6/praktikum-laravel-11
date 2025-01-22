21b57b975ca0af23d7ac7482a0883ae5cd030ab2
muhamadabelldeskiawan <muhamadabelldeskiawan@gmail.com>
2025-01-22 20:37:50 +0800

    commit ketiga login

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\HomeController;
 use App\Models\Category;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+Route::get('home', [HomeController::class, 'home'])->name('home')->middleware('auth');
+Route::get('registeruser', [RegisterController::class, 'register'])->name('register');
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
 
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
 Route::resource('/categories', \App\Http\Controllers\CategoryController::class);
